2423f150d3e899b4246b8b302bf52aa953fd88b9
SalenCoin <36447267+SalenCoin@users.noreply.github.com>
2018-02-22 14:39:14 +0300

    Update bitcoin.cpp

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -298,7 +298,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("bitcoin.sipa.be", 8333, true);
+  CService ip("seednode1.salencoin.com", 9103, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
